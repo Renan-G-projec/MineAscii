c1a9d4fa9e2a86601a83f1a373b3e6b1f0caad32
Renan-G-projec <renanfartes@gmail.com>
2026-08-02 09:46:53 -0300

    feat: Added GLAD support. AMDG

--- src/main.c
+++ src/main.c
@@ -4,17 +4,12 @@
 #include "asciibuffer.h"
 
 int main() {
-    struct asciibuffer ascbuff;
-    asciibuffer_init(&ascbuff, 10, 10);
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-            asciibuffer_setbgcolor(&ascbuff, 0, 0, x == y ? 255 : 0);
-            asciibuffer_writechar(&ascbuff, ' ');
-        }
-        asciibuffer_writechar(&ascbuff, '\n');
-    }
+    glfwInitHint(GLFW_VISIBLE, GLFW_TRUE);
+    if (!glfwInit()) {
+        printf("Error initializing GLFW.\nExiting...\n");
+        return 0;
+    };
 
-    asciibuffer_print(&ascbuff);
-    asciibuffer_destroy(&ascbuff);
+    glfwTerminate();
     return 0;
 }

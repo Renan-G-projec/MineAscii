b71cc57ad51fc6cfe93760857c2b86509bf9424b
Renan-G-projec <renanfartes@gmail.com>
2026-08-02 10:01:10 -0300

    feat: Basic window created. AMDG

--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
 // Ad Maiorem Dei Gloriam!
 #include <stdio.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "asciibuffer.h"
 
 int main() {
-    glfwInitHint(GLFW_VISIBLE, GLFW_TRUE);
     if (!glfwInit()) {
         printf("Error initializing GLFW.\nExiting...\n");
         return 0;
     };
 
+    GLFWwindow* window = glfwCreateWindow(100, 100, "Hello, window! - AMDG", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }

1e6768ebf03e5143a56a56e9d6d339ccf4e8d3b4
Renan-G-projec <renanfartes@gmail.com>
2026-08-04 21:07:26 -0300

    feat: Added EBO interface. AMDG

---
+++ include/ebo.h
@@ -0,0 +1,24 @@
+// Ad Maiorem Dei Gloriam!
+#ifndef EBO_H
+#define EBO_H
+
+#include <glad/glad.h>
+
+typedef GLuint EBO;
+
+// Creates an element buffer
+EBO ebo_create(void);
+
+// Copies the EBO vertices
+void ebo_send_data(EBO, GLsizei size, GLfloat *data);
+
+// Binds the EBO
+void ebo_bind(EBO);
+
+// Unbinds it
+void ebo_unbind(void);
+
+// Destroys and frees the memory of the element buffer
+void ebo_destroy(EBO);
+
+#endif

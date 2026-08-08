baedb4f51a45860e22ea93be99663c8da33f7728
Renan-G-projec <renanfartes@gmail.com>
2026-08-08 13:55:52 -0300

    chore: Texture interface created. AMDG

---
+++ include/texture.h
@@ -0,0 +1,25 @@
+// Ad Maiorem Dei Gloriam!
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <stdint.h>
+#include <glad/glad.h>
+#include <stb_image.h>
+
+typedef GLuint Texture;
+
+// Initializes the texture
+Texture texture_create(void);
+
+// Loads from a file with stb image
+uint8_t texture_load_png(Texture);
+
+// Binds the texture
+void texture_bind(Texture);
+
+void texture_unbind(void);
+
+// Destroys the texture and frees from vram
+void texture_destroy(Texture);
+
+#endif

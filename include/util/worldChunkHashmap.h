d508a15643c2b241cc71ff8d0a86a285bfa72191
Renan-G-projec <renanfartes@gmail.com>
2026-08-19 10:04:19 -0300

    chore: Added Hashmap for future chunk runtime generation. AMDG

---
+++ include/util/worldChunkHashmap.h
@@ -0,0 +1,31 @@
+// Ad Maiorem Dei Gloriam!
+#ifndef WORLD_CHUNK_HASHMAP_H
+#define WORLD_CHUNK_HASHMAP_H
+
+#include "core/chunk.h"
+
+#define NUM_BUCKETS 100
+
+struct ChunkHashmapNode {
+    WorldPos key;
+    Chunk chunk;
+    struct ChunkHashmapNode *next;
+};
+
+typedef struct {
+    struct ChunkHashmapNode *buckets[NUM_BUCKETS];
+} ChunkHashmap;
+
+// Creates the chunk hashmap and returns it
+ChunkHashmap chunkhashmap_create();
+
+// Frees all the memory and sets every bucket to NULL
+void chunkhashmap_clear(ChunkHashmap *);
+
+// Gets the chunk by the world pos
+Chunk *chunkhashmap_get(ChunkHashmap *, WorldPos key);
+
+// Sets the chunk by the world pos
+void *chunkhashmap_set(ChunkHashmap *, WorldPos key, Chunk);
+
+#endif
---
+++ src/util/worldChunkHashmap.c
@@ -0,0 +1,70 @@
+// Ad Maiorem Dei Gloriam!
+#include "util/worldChunkHashmap.h"
+
+static inline unsigned int hash_world_pos(WorldPos worldPos) {
+    const unsigned long long k1 = 14020252567694004749ULL;
+    const unsigned long long k2 = 9695998579374968831ULL;
+    const unsigned long long k3 = 2860486313ULL;
+
+    unsigned long long hash = k3;
+    hash = (hash ^ (unsigned long long)worldPos.x) * k1;
+    hash = (hash ^ (unsigned long long)worldPos.y) * k2;
+    hash = (hash ^ (unsigned long long)worldPos.z) * k1;
+
+    hash ^= hash >> 33;
+    hash *= 0xff51afd7ed558ccdULL;
+    hash ^= hash >> 33;
+
+    return hash % NUM_BUCKETS;
+}
+
+static inline bool world_pos_compare(WorldPos a, WorldPos b) {
+    return (a.x == b.x && a.y == b.y && a.z == b.z);
+}
+
+ChunkHashmap chunkhashmap_create() {
+    ChunkHashmap chunkHashmap;
+
+    for (int i = 0; i < NUM_BUCKETS; ++i) {
+        chunkHashmap.buckets[i] = NULL;
+    }
+
+    return chunkHashmap;
+}
+
+void chunkhashmap_clear(ChunkHashmap *chunkHashmap) {
+    for (int i =0; i < NUM_BUCKETS; ++i) {
+        struct ChunkHashmapNode *current = chunkHashmap->buckets[i];
+        while (current) {
+            chunk_destroy(&current->chunk);
+            struct ChunkHashmapNode *next = current->next;
+            current->next = NULL;
+            free(current);
+            current = next;
+        }
+        chunkHashmap->buckets[i] = NULL;
+    }
+}
+
+Chunk *chunkhashmap_get(ChunkHashmap *chunkHashmap, WorldPos key) {
+    struct ChunkHashmapNode *node = chunkHashmap->buckets[hash_world_pos(key)];
+
+    while (node) {
+        if (world_pos_compare(key, node->key)) return &node->chunk;
+        node = node->next;
+    }
+
+    return NULL;
+}
+
+void *chunkhashmap_set(ChunkHashmap *chunkHashmap, WorldPos key, Chunk chunk) {
+    unsigned int chunkNodeIndex = hash_world_pos(key);
+    struct ChunkHashmapNode *node = chunkHashmap->buckets[chunkNodeIndex];
+
+    struct ChunkHashmapNode *newOne = (struct ChunkHashmapNode *)malloc(sizeof(struct ChunkHashmapNode));\
+    newOne->chunk = chunk;
+    newOne->key = key;
+    newOne->next = node;
+
+    chunkHashmap->buckets[chunkNodeIndex] = newOne;
+};
